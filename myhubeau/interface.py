50352efcecd28d969914bf8a1d8e0d9d1a3d718b
Thibault Hallouin <thibhlln@gmail.com>
2023-12-08 11:41:02 +0100

    refactor hydrometry to collect available stations only once

--- myhubeau/interface.py
+++ myhubeau/interface.py
@@ -4,7 +4,11 @@ import numpy as np
 import re
 
 
-def _get_data(url, data):
+# "global" (module-wide) variables for memoization
+_hydrometry_stations = None
+
+
+def _get_json_data(url: str, data: list) -> list:
     r = requests.get(url)
 
     if r.status_code == 200:
@@ -12,57 +16,71 @@ def _get_data(url, data):
         return data
     elif r.status_code == 206:
         data.extend(r.json()['data'])
-        return _get_data(r.json()['next'], data)
+        return _get_json_data(r.json()['next'], data)
     else:
         raise RuntimeError(
             f"data retrieval failed for query with URL {url}"
         )
 
 
-def _get_dataframe(endpoint: str, operation: str, parameters: dict):
-    _url = (
+def _get_dataframe(endpoint: str, operation: str, parameters: dict) -> pd.DataFrame or None:
+    url = (
         f"https://hubeau.eaufrance.fr/api/{endpoint}/{operation}?"
         f"{'&'.join(['='.join([k, str(v)]) for k, v in parameters.items()])}"
     )
 
-    data = []
+    data = _get_json_data(url, [])
 
-    data = _get_data(_url, data)
+    if data:
+        return pd.concat(
+            [pd.DataFrame.from_dict(i, orient='index') for i in data],
+            axis=1
+        ).T
 
-    return pd.concat(
-        [pd.DataFrame.from_dict(i, orient='index') for i in data],
-        axis=1
-    ).T
 
+def _set_and_get_hydrometry_stations() -> list:
+    global _hydrometry_stations
 
-def get_hydrometry(code_station: str):
-    # get list of available stations still on operation
-    stations = _get_dataframe(
+    # set list of available stations still in operation
+    hydrometry_stations = _get_dataframe(
         endpoint="v1/hydrometrie",
         operation="referentiel/stations",
         parameters={
-            'fields': 'code_station,en_service',
+            'fields': 'code_station',
+            'en_service': 'true',
             'size': 10000
         }
     )
 
+    # check whether at least one station exists
+    if hydrometry_stations is not None:
+        _hydrometry_stations = (
+            hydrometry_stations['code_station'].values.tolist()
+        )
+    else:
+        _hydrometry_stations = []
+
+    return _hydrometry_stations
+
+
+def get_hydrometry(code_station: str) -> pd.DataFrame or None:
+    # collect list of hydrometric stations (if not already collected)
+    hydrometry_stations = (
+        _hydrometry_stations if _hydrometry_stations is not None
+        else _set_and_get_hydrometry_stations()
+    )
+
     # check code station is available
-    if code_station not in stations['code_station'].values:
+    if code_station not in hydrometry_stations:
         raise ValueError(
             f"code station {repr(code_station)} is not "
-            f"available in Hub'Eau hydrometry API"
+            f"available in Hub'Eau hydrometry API or "
+            f"is not in operation anymore"
         )
 
-    # check station is still in operation
-    if code_station not in (
-            stations[stations['en_service']]['code_station'].values
-    ):
-        raise ValueError(
-            f"code station {repr(code_station)} is not "
-            f"in operation anymore"
-        )
-
+    # ------------------------------------------------------------------
     # get elaborated data
+    # ------------------------------------------------------------------
     try:
         data_elab = _get_dataframe(
             endpoint="v1/hydrometrie",
@@ -82,13 +100,23 @@ def get_hydrometry(code_station: str):
             f"for {code_station}"
         ) from e
 
-    # check elaborated data quality
-    data_elab['resultat_obs_elab'][
-        np.isin(data_elab['code_statut'].values, ('16', '20'))
-    ] = np.nan
-    data_elab.drop(columns='code_statut', inplace=True)
+    if data_elab is not None:
+        # check elaborated data quality
+        data_elab['resultat_obs_elab'][
+            np.isin(data_elab['code_statut'].values, ('16', '20'))
+        ] = np.nan
+        data_elab.drop(columns='code_statut', inplace=True)
 
+        # convert to datetime
+        data_elab['date_obs_elab'] = pd.to_datetime(
+            data_elab['date_obs_elab'], format='%Y-%m-%d'
+        )
+        # rename headers
+        data_elab.columns = ['Date', 'Debit']
+
+    # ------------------------------------------------------------------
     # get real time data
+    # ------------------------------------------------------------------
     try:
         data_tr = _get_dataframe(
             endpoint="v1/hydrometrie",
@@ -108,24 +136,25 @@ def get_hydrometry(code_station: str):
             f"for {code_station}"
         ) from e
 
-    # aggregate real-time data to mean daily values
-    data_tr['date_obs'] = pd.to_datetime(
-        data_tr['date_obs'], format='%Y-%m-%dT%H:%M:%SZ'
-    )
-    data_tr.set_index('date_obs', inplace=True)
-    data_tr = data_tr.resample('D').agg('mean')
-    data_tr.reset_index(inplace=True)
+    if data_tr is not None:
+        # aggregate real-time data to mean daily values
+        data_tr['date_obs'] = pd.to_datetime(
+            data_tr['date_obs'], format='%Y-%m-%dT%H:%M:%SZ'
+        )
+        data_tr.set_index('date_obs', inplace=True)
+        data_tr = data_tr.resample('D').agg('mean')
+        data_tr.reset_index(inplace=True)
 
-    # merge datasets
-    data_elab['date_obs_elab'] = pd.to_datetime(
-        data_elab['date_obs_elab'], format='%Y-%m-%d'
-    )
-    data_elab.columns = ['Date', 'Debit']
-    data_tr.columns = ['Date', 'Debit']
+        # rename headers
+        data_tr.columns = ['Date', 'Debit']
 
-    data = pd.concat([data_elab, data_tr])
+    # ------------------------------------------------------------------
+    # return potentially aggregated elaborated and/or real-time data
+    # ------------------------------------------------------------------
+    if (data_elab is not None) or (data_tr is not None):
+        data = pd.concat([data_elab, data_tr]).reset_index(drop=True)
+        return data
 
-    return data.reset_index(drop=True)
 
 
 def get_piezometry(code_bss: str):
