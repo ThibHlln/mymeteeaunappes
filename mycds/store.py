e97c510d6859d5af0effaab8c71ca42f5d8598aa
Thibault Hallouin <thibhlln@gmail.com>
2023-12-15 17:54:04 +0100

    add possibly to set start/end for data period
    if before/after available record, filled with missing values

--- mycds/store.py
+++ mycds/store.py
@@ -1,11 +1,14 @@
 import os
+from datetime import datetime
+import pandas as pd
+import numpy as np
 
 from .collect import get_meteorology
 
 
 def save_meteorology(
         latitude: float, longitude: float, working_dir: str,
-        update_era5_land=False
+        start: str = None, end: str = None, update_era5_land=False
 ) -> None:
     # collect dataset
     ds = get_meteorology(latitude, longitude, update_era5_land)
@@ -22,6 +25,20 @@ def save_meteorology(
     df['Pluie'] *= 1000  # m to mm
     df['ETP'] *= 1000  # m to mm
     df['Temperature'] -= 273.15  # K to degC
+
+    # adjust period to match start and end dates if provided
+    start = (
+        df['Date'].iloc[0] if (start is None)
+        else datetime.strptime(start, '%Y-%m-%d')
+    )
+    end = (
+        df['Date'].iloc[-1] if (end is None)
+        else datetime.strptime(end, '%Y-%m-%d')
+    )
+
+    df = df.set_index('Date')
+    df = df.reindex(pd.date_range(start, end), fill_value=np.nan)
+    df = df.reset_index(names='Date')
 
     # format date to "Excel date"
     df['Date'] = df['Date'].dt.strftime('%d/%m/%Y')
--- myhubeau/store.py
+++ myhubeau/store.py
@@ -1,14 +1,29 @@
 import os
 import numpy as np
 import pandas as pd
+from datetime import datetime
 
 from .collect import get_hydrometry, get_piezometry, get_withdrawal
 
 
 def _save_df_as_prn_file(
         df: pd.DataFrame, working_dir: str, measure_label: str,
-        missing_value: float
+        missing_value: float, start: str = None, end: str = None
 ):
+    # adjust period to match start and end dates if provided
+    start = (
+        df['Date'].iloc[0] if (start is None)
+        else datetime.strptime(start, '%Y-%m-%d')
+    )
+    end = (
+        df['Date'].iloc[-1] if (end is None)
+        else datetime.strptime(end, '%Y-%m-%d')
+    )
+
+    df = df.set_index('Date')
+    df = df.reindex(pd.date_range(start, end), fill_value=np.nan)
+    df = df.reset_index(names='Date')
+
     # format date to "Excel date"
     df['Date'] = df['Date'].dt.strftime('%d/%m/%Y')
 
@@ -27,27 +42,32 @@ def _save_df_as_prn_file(
 
 
 def save_hydrometry(
-        code_station: str, working_dir: str, include_realtime: bool = True
+        code_station: str, working_dir: str,
+        start: str = None, end: str = None,
+        include_realtime: bool = True
 ):
     # collect data as dataframe
     df = get_hydrometry(code_station, include_realtime)
 
     # store as PRN file
-    _save_df_as_prn_file(df, working_dir, 'Debit', -2)
+    _save_df_as_prn_file(df, working_dir, 'Debit', -2, start, end)
 
 
 def save_piezometry(
-        code_bss: str, working_dir: str, include_realtime: bool = True
+        code_bss: str, working_dir: str,
+        start: str = None, end: str = None,
+        include_realtime: bool = True
 ):
     # collect data as dataframe
     df = get_piezometry(code_bss, include_realtime)
 
     # store as PRN file
-    _save_df_as_prn_file(df, working_dir, 'Niveau', 9999)
+    _save_df_as_prn_file(df, working_dir, 'Niveau', 9999, start, end)
 
 
 def save_withdrawal(
-        code_ouvrage: str, working_dir: str
+        code_ouvrage: str, working_dir: str,
+        start: str = None, end: str = None
 ):
     # collect data as dataframe
     df = get_withdrawal(code_ouvrage)
@@ -66,4 +86,4 @@ def save_withdrawal(
     df[measure_label] = df[measure_label].round(3)
 
     # store as PRN file
-    _save_df_as_prn_file(df, working_dir, measure_label, np.nan)
+    _save_df_as_prn_file(df, working_dir, measure_label, np.nan, start, end)
