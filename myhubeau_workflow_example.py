376a2ed2c91e0a438eef536cef7ad7d2b0db9336
Thibault Hallouin <thibhlln@gmail.com>
2023-12-20 15:41:55 +0100

    update stations used for example for consistency

--- myhubeau_workflow_example.py
+++ myhubeau_workflow_example.py
@@ -6,19 +6,19 @@ from myhubeau.store import (
 working_dir = 'examples/my-example'
 
 save_hydrometry(
-    code_station='M107302001', working_dir=working_dir,
-    filename='debit-M107302001.prn',
-    start='1990-01-01', end='2023-12-01'
+    code_station='E642601001', working_dir=working_dir,
+    filename='debit-E642601001.prn',
+    start='1985-01-01', end='2023-07-31'
 )
 
 save_piezometry(
-    code_bss='06301X0131/F', working_dir=working_dir,
-    filename='niveau-06301X0131_F.prn',
-    start='1990-01-01', end='2023-12-01'
+    code_bss='00608X0028/S1', working_dir=working_dir,
+    filename='niveau-00608X0028_S1.prn',
+    start='1985-01-01', end='2023-07-31'
 )
 
 save_withdrawal(
-    code_ouvrage='OPR0000000003', working_dir=working_dir,
-    filename='prelevement-OPR0000000003.prn',
-    start='1990-01-01', end='2023-12-01'
+    code_ouvrage='OPR0000041735', working_dir=working_dir,
+    filename='prelevement-OPR0000041735.prn',
+    start='1985-01-01', end='2023-07-31'
 )
