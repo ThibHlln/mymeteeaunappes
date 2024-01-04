a5e32554ef7c873bad1944bb6b1a3b5a7fb2871a
Thibault Hallouin <thibhlln@gmail.com>
2024-01-04 12:11:47 +0100

    update example for mymeteofrance

--- mymeteofrance_workflow_example.py
+++ mymeteofrance_workflow_example.py
@@ -4,8 +4,10 @@ from mymeteofrance.collect import get_meteorology
 key = 'paste your API key here'
 
 df = get_meteorology(
-    variables=['RR'],
+    variables=['RR', 'ETPMON', 'TM'],
     station_id=20004002,
+    start='2021-01-01 00:00:00', end='2022-12-31 00:00:00',
+    check_station_id=False,
     api_key=key
 )
 
